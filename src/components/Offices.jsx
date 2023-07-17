a37962f85053ae704113ac2ed4ed2034721c3107
demarijm <demarijmiller@gmail.com>
2023-07-17 11:54:45 -0400

    New comp added from shortcut

---
+++ src/components/Offices.jsx
@@ -0,0 +1,32 @@
+import clsx from 'clsx'
+
+function Office({ name, children, invert = false }) {
+  return (
+    <address
+      className={clsx(
+        'text-sm not-italic',
+        invert ? 'text-neutral-300' : 'text-neutral-600'
+      )}
+    >
+      <strong className={invert ? 'text-white' : 'text-neutral-950'}>
+        {name}
+      </strong>
+      <br />
+      {children}
+    </address>
+  )
+}
+
+export function Offices({ invert = false, ...props }) {
+  return (
+    <ul role="list" {...props}>
+      <li>
+        <Office name="Headquarters" invert={invert}>
+          6001 Cass Ave
+          <br />
+          Detroit, Michigan
+        </Office>
+      </li>
+    </ul>
+  )
+}
