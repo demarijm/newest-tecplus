26a8024a3a1f0394870b371c22ea35b5daec5a54
demarijm <demarijmiller@gmail.com>
2023-08-13 13:42:37 -0400

    New comp added from shortcut

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -86,14 +86,14 @@ function Header({
             aria-expanded={expanded.toString()}
             aria-controls={panelId}
             className={clsx(
-              'group -m-2.5 bg-red-600 rounded-full p-2.5 transition',
+              'group -m-2.5  rounded-full p-2.5 transition',
               invert ? 'hover:bg-white/10' : 'hover:bg-neutral-950/10'
             )}
             aria-label="Toggle navigation"
           >
             <Icon
               className={clsx(
-                'h-6 w-6 bg-blue-400',
+                'h-6 w-6 ',
                 invert
                   ? 'fill-white group-hover:fill-neutral-200'
                   : 'fill-neutral-950 group-hover:fill-neutral-700'
